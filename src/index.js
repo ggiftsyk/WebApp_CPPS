ae537116e70836de691a2654bd630fca779e84d2
siriyakorn achasewin <88705838+ggiftsyk@users.noreply.github.com>
2024-09-21 15:45:39 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import DetailPage from './DetailPage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DetailPage />
+    <DetailPages/>
   </React.StrictMode>
 );
 
